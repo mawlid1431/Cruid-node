f6d4c114551378088504f82d208f6d1b06ceef34
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-06-10 12:10:24 +0300

    added a new dta

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,33 @@
 import express from "express";
 const router = express.Router();
 
+import { v4 as uuidv4 } from 'uuid';
+
+
 const userz = [
-  {
-    name: 'Ahmed',
-    nationality: 'Somalilander',
-    city: 'Hargeisa',
-    age: 45
-  },
-  {
-    name: 'muse',
-    nationality: 'Somalilander',
-    city: 'Hargeisa',
-    age: 24
-  },
-  {
-    name: 'Malit',
-    nationality: 'Somalilander',
-    city: 'Hargeisa',
-    age: 55
-  }
+
+
+
+  //this data or object was the manual data but now you can aad the 
+  // postman and you willl inside this array 
+ // {
+  //   name: 'Ahmed',
+  //   nationality: 'Somalilander',
+  //   city: 'Hargeisa',
+  //   age: 45
+  // },
+  // {
+  //   name: 'muse',
+  //   nationality: 'Somalilander',
+  //   city: 'Hargeisa',
+  //   age: 24
+  // },
+  // {
+  //   name: 'Malit',
+  //   nationality: 'Somalilander',
+  //   city: 'Hargeisa',
+  //   age: 55
+  // }
 ]
 
 router.get("/", (req, res) => {
@@ -28,7 +36,16 @@ router.get("/", (req, res) => {
 
 router.post('/', (req, res) => {
   const newUser = req.body;
-  userz.push(newUser);
+
+  const userId = uuidv4();
+
+  const userWithid = {
+    ...newUser,
+    id: userId
+  }
+
+
+  userz.push(userWithid);
   res.send(`User with the username ${newUser.name} added to the database.`);
 });
 
